99b832f7e5d1aece4288c3633ec683f86c17ca72
Nikita <filippovni1999@gmail.com>
2023-03-25 23:38:59 +0300

    feat: Add two functions saveUser() and getUser() to the UserSettings class and add a constructor to the User class

--- app/src/main/java/com/example/appmaga/User/User.java
+++ app/src/main/java/com/example/appmaga/User/User.java
@@ -3,6 +3,11 @@ package com.example.appmaga.User;
 public class User {
     private String login, password;
 
+    public User(String login, String password){
+        this.login = login;
+        this.password = password;
+    }
+
     public String getLogin() {
         return login;
     }
