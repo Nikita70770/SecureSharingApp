af2cb9a83b9bfba7d86e0d7027dadc0d0329985c
Nikita <filippovni1999@gmail.com>
2023-03-27 14:59:23 +0300

    feat: Make small changes to the style of the side menu as well as initialize and configure the side menu in the MainActivity class

--- app/src/main/java/com/example/appmaga/Activities/MainActivity.java
+++ app/src/main/java/com/example/appmaga/Activities/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.appmaga.Activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.example.appmaga.R;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initElements();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void initElements(){
+        drawerLayout = findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        navigationView = findViewById(R.id.navigationView);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setItemIconTintList(null);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
