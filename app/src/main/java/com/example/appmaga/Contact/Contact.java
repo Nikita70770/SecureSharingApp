bec355521356d7e6004344ec370069747609e128
Nikita <filippovni1999@gmail.com>
2023-03-31 05:29:45 +0300

    feat: Implement the Contact class that represents the data model displayed by RecyclerView

---
+++ app/src/main/java/com/example/appmaga/Contact/Contact.java
@@ -0,0 +1,17 @@
+package com.example.appmaga.Contact;
+
+public class Contact {
+    private String loginContact, passwordContact, randValueContact;
+
+    public String getLoginContact() {
+        return loginContact;
+    }
+
+    public String getPasswordContact() {
+        return passwordContact;
+    }
+
+    public String getRandValueContact() {
+        return randValueContact;
+    }
+}
