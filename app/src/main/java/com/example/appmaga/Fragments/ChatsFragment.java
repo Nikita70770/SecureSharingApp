b662c6a39965d5ac80896fef906c28aed224655c
Nikita <filippovni1999@gmail.com>
2023-03-31 07:48:49 +0300

    feat: Add the ChatsFragment class to the project

---
+++ app/src/main/java/com/example/appmaga/Fragments/ChatsFragment.java
@@ -0,0 +1,12 @@
+package com.example.appmaga.Fragments;
+
+import java.util.List;
+
+public class ChatsFragment {
+
+    private List<String> listContacts;
+
+    public ChatsFragment(List<String> list){
+        this.listContacts = list;
+    }
+}
