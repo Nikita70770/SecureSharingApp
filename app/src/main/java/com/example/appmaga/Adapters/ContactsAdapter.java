1e1a25366c76432638bd9162c6e77fd2d88abdc8
Nikita <filippovni1999@gmail.com>
2023-03-31 06:44:52 +0300

    feat: Create a constructor for the ContactsAdapter class

--- app/src/main/java/com/example/appmaga/Adapters/ContactsAdapter.java
+++ app/src/main/java/com/example/appmaga/Adapters/ContactsAdapter.java
@@ -1,5 +1,6 @@
 package com.example.appmaga.Adapters;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,9 +8,21 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.appmaga.Contact.Contact;
 import com.example.appmaga.R;
 
+import java.util.List;
+
 public class ContactsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    private Context context;
+    private List<Contact> contacts;
+
+    public ContactsAdapter(Context context, List<Contact> list){
+        this.context = context;
+        this.contacts = list;
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
