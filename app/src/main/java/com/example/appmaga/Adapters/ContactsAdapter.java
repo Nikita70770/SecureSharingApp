40b3c36b3b43d488cb4022622ed8e2394a3ceb85
Nikita <filippovni1999@gmail.com>
2023-03-31 06:36:17 +0300

    feat: Create a ChatViewHolder class whose objects the adapter will use to store and visualize list items

--- app/src/main/java/com/example/appmaga/Adapters/ContactsAdapter.java
+++ app/src/main/java/com/example/appmaga/Adapters/ContactsAdapter.java
@@ -1,9 +1,13 @@
 package com.example.appmaga.Adapters;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appmaga.R;
 
 public class ContactsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     @NonNull
@@ -20,5 +24,14 @@ public class ContactsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public class ChatViewHolder extends RecyclerView.ViewHolder{
+        TextView txtLoginContact;
+
+        public ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtLoginContact = itemView.findViewById(R.id.txtLoginContact);
+        }
     }
 }
