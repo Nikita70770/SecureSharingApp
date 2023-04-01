ed3235ddda94fc791ee94c00be7539c206b8410a
Nikita <filippovni1999@gmail.com>
2023-04-01 22:48:14 +0300

    feat: Add onCreateView() function to the ChatsFragment class

--- app/src/main/java/com/example/appmaga/Fragments/ChatsFragment.java
+++ app/src/main/java/com/example/appmaga/Fragments/ChatsFragment.java
@@ -1,14 +1,27 @@
 package com.example.appmaga.Fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
 import androidx.fragment.app.Fragment;
 
+import com.example.appmaga.R;
 import java.util.List;
 
 public class ChatsFragment extends Fragment {
-
     private List<String> listContacts;
 
     public ChatsFragment(List<String> list){
         this.listContacts = list;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.chat_list_fragment, container, false);
+        return view;
     }
 }
