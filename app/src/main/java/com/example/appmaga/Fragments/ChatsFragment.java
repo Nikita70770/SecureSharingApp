4e35290c51db7793fc38e37666b7402fcd1e3f93
Nikita <filippovni1999@gmail.com>
2023-04-01 23:04:31 +0300

    feat: Add the initAdapter() function to the ChatsFragment class

--- app/src/main/java/com/example/appmaga/Fragments/ChatsFragment.java
+++ app/src/main/java/com/example/appmaga/Fragments/ChatsFragment.java
@@ -7,15 +7,24 @@ import android.view.ViewGroup;
 
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.appmaga.Adapters.ContactsAdapter;
+import com.example.appmaga.Contact.Contact;
+import com.example.appmaga.Gson.GsonWork;
 import com.example.appmaga.R;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ChatsFragment extends Fragment {
-    private List<String> listContacts;
+    private List<Contact> listContacts;
+    private RecyclerView chatListRecyclerview;
+    private ContactsAdapter adapter;
 
     public ChatsFragment(List<String> list){
-        this.listContacts = list;
+        this.listContacts = getContacts(list);
     }
 
     @Override
@@ -23,5 +32,12 @@ public class ChatsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.chat_list_fragment, container, false);
         return view;
+    }
+
+    private void initContactsAdapter(View view){
+        chatListRecyclerview = view.findViewById(R.id.chatListRecyclerview);
+        adapter = new ContactsAdapter(getContext(), listContacts);
+        chatListRecyclerview.setAdapter(adapter);
+        chatListRecyclerview.setLayoutManager(new LinearLayoutManager(this.getContext()));
     }
 }
