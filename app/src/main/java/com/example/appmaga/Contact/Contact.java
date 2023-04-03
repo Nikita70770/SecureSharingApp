b3a6d5fd35e948271bf90e0359fc1eb7bcfdf555
Nikita <filippovni1999@gmail.com>
2023-04-03 23:10:14 +0300

    feat: Add a constructor to the Contact class

--- app/src/main/java/com/example/appmaga/Contact/Contact.java
+++ app/src/main/java/com/example/appmaga/Contact/Contact.java
@@ -3,6 +3,12 @@ package com.example.appmaga.Contact;
 public class Contact {
     private String login, password, randValue;
 
+    public Contact(String login, String password, String randN){
+        this.login = login;
+        this.password = password;
+        this.randValue = randN;
+    }
+
     public String getLogin() {
         return login;
     }
