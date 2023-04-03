aa5c5d2bff76b6e150f62d3b820a538b8f7b32b5
Nikita <filippovni1999@gmail.com>
2023-04-03 23:28:03 +0300

    feat: Implement another static newInstance() function in the ChatsFragment class without arguments

--- app/src/main/java/com/example/appmaga/Fragments/ChatsFragment.java
+++ app/src/main/java/com/example/appmaga/Fragments/ChatsFragment.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -36,6 +34,14 @@ public class ChatsFragment extends Fragment implements IChatsFragmentListener, C
         ChatsFragment chatsFragment = new ChatsFragment();
         Bundle args = new Bundle();
         args.putStringArrayList(ConstantKeysFragment.DATA_CONTACTS_KEY, new ArrayList<>(list));
+        chatsFragment.setArguments(args);
+        return chatsFragment;
+    }
+
+    public static ChatsFragment newInstance(){
+        ChatsFragment chatsFragment = new ChatsFragment();
+        Bundle args = new Bundle();
+        args.putStringArrayList(ConstantKeysFragment.DATA_CONTACTS_KEY, new ArrayList<>());
         chatsFragment.setArguments(args);
         return chatsFragment;
     }
