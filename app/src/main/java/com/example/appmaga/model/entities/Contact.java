fd5ec549867aa23d9505e2567bfab3549dd9153e
Nikita <filippovni1999@gmail.com>
2023-04-15 10:34:33 +0300

    feat: Use the Contact class as an entity to create a contact table

--- app/src/main/java/com/example/appmaga/model/entities/Contact.java
+++ app/src/main/java/com/example/appmaga/model/entities/Contact.java
@@ -1,12 +1,32 @@
 package com.example.appmaga.model.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "contacts")
 public class Contact {
-    private String login, password, randValue;
+
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+
+    private String login;
+
+    private String password;
+
+    private String randValue;
 
     public Contact(String login, String password, String randN){
         this.login = login;
         this.password = password;
         this.randValue = randN;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getLogin() {
