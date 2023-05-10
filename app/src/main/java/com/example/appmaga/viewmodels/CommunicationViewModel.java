dc660c5e86aea01124cd987c18eba4dc363a021a
Nikita <filippovni1999@gmail.com>
2023-05-11 00:32:33 +0300

    feat: Add a new openWindowWithMessengers() function to the CommunicationViewModel class

--- app/src/main/java/com/example/appmaga/viewmodels/CommunicationViewModel.java
+++ app/src/main/java/com/example/appmaga/viewmodels/CommunicationViewModel.java
@@ -1,6 +1,8 @@
 package com.example.appmaga.viewmodels;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -10,13 +12,23 @@ import com.example.appmaga.model.repository.DataRepository;
 public class CommunicationViewModel extends AndroidViewModel {
 
     private DataRepository repository;
+    private Context context;
 
     public void init(){
+        this.context = getApplication().getBaseContext();
         this.repository = new DataRepository(getApplication().getBaseContext());
         this.repository.setPreferencesInstance();
     }
 
     public CommunicationViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    private void openWindowWithMessengers(String message){
+        final Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        intent.setType("*/*");
+        context.startActivity(Intent.createChooser(intent, "Share with friends"));
     }
 }
