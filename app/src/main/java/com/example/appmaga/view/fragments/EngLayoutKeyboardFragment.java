552bd320ae321c3ab5ab1b4b52863aafc93b6aba
Nikita <filippovni1999@gmail.com>
2023-05-20 16:15:35 +0300

    Override the onClick() method in the EngLayoutKeyboardFragment class and create the body of the initViewElements() function

--- app/src/main/java/com/example/appmaga/view/fragments/EngLayoutKeyboardFragment.java
+++ app/src/main/java/com/example/appmaga/view/fragments/EngLayoutKeyboardFragment.java
@@ -2,6 +2,7 @@ package com.example.appmaga.view.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,7 +21,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class EngLayoutKeyboardFragment extends Fragment {
+public class EngLayoutKeyboardFragment extends Fragment implements View.OnClickListener {
 
     private Button btnSpecialCharacters, btnSpace;
     private ImageButton btnDelete, btnEnter;
@@ -49,7 +50,7 @@ public class EngLayoutKeyboardFragment extends Fragment {
             R.id.button_39, R.id.button_40, R.id.button_41, R.id.button_42, R.id.button_43, R.id.button_44,
             R.id.button_45, R.id.button_46, R.id.button_47, R.id.button_48, R.id.button_49, R.id.button_50,
             R.id.button_51, R.id.button_52, R.id.button_53, R.id.button_54, R.id.button_55, R.id.button_56,
-            R.id.button_57, R.id.button_58, R.id.button_59, R.id.button_60, R.id.button_61
+            R.id.button_57, R.id.button_58, R.id.button_59, R.id.button_60
     };
 
     private IKeyboardListener keyboardListener;
@@ -89,7 +90,56 @@ public class EngLayoutKeyboardFragment extends Fragment {
     }
 
     private void initViewElements(View view) {
+        for(int i = 0; i < BUTTONS_IDS.length; i++){
+            int id = BUTTONS_IDS[i];
+            Button btn = view.findViewById(id);
+            btn.setOnClickListener(this);
+            keyValues.put(id, engLayout.get(i));
+        }
+
+        btnSpecialCharacters = view.findViewById(R.id.button_special_characters);
+        btnSpace = view.findViewById(R.id.button_space);
+        btnDelete = view.findViewById(R.id.button_delete);
+        btnEnter = view.findViewById(R.id.button_enter);
+
+        btnSpecialCharacters.setOnClickListener(this);
+        btnSpace.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+        btnEnter.setOnClickListener(this);
+
+        btnSpace.setOnTouchListener(swipeTouchListener);
+
+        keyValues.put(R.id.button_space, " ");
+        keyValues.put(R.id.button_enter, "\n");
     }
 
+    @Override
+    public void onClick(View v) {
+        // do nothing if the InputConnection has not been set yet
+        if (inputConnection == null) return;
 
+        switch (v.getId()){
+            case R.id.button_delete:
+                if(listCodes.size() > 0) listCodes.remove(listCodes.size()-1);
+
+                CharSequence selectedText = inputConnection.getSelectedText(0);
+                if (TextUtils.isEmpty(selectedText)) {
+                    // no selection, so delete previous character
+                    inputConnection.deleteSurroundingText(1, 0);
+                }
+                else {
+                    // delete the selection
+                    inputConnection.commitText("", 1);
+                }
+                break;
+
+            case R.id.button_special_characters:
+                break;
+
+            default:
+                String value = keyValues.get(v.getId());
+                inputConnection.commitText(value, 1);
+                break;
+        }
+    }
 }
